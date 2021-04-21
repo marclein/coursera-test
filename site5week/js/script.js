219f137a2691ebef97eb7ce1c3bbbaa247327eec
Marlon Suárez <inperios@gmail.com>
2021-04-20 21:02:32 -0500

    some correction

--- site5week/js/script.js
+++ site5week/js/script.js
@@ -115,15 +115,15 @@ function buildAndShowHomeHTML (categories) {
       // Hint: you need to surround the chosen category short name with something before inserting
       // it into the home html snippet.
       //
-      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtmlToInsertIntoMainPage, 
+      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, 
       									"randomCategoryShortName", 
-      									chosenCategoryShortName);.
+      									chosenCategoryShortName);
 
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
-      insertHtml("#main-content",homeHtmlToInsertIntoMainPage)
+      insertHtml("#main-content", homeHtmlToInsertIntoMainPage)
 
     },
     false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
